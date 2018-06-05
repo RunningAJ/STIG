acfd462356bf125954b51ad851b4f66e2b39fb5d
Running AJ <oifveteran06@gmail.com>
2018-06-04 22:33:45 -0600

    Add files via upload

---
+++ SCAP_Aggregator.ps1
@@ -0,0 +1,76 @@
+﻿######################################################################################
+#
+# Chief J's SCAP Agrregator
+# Version 2 / 30MAY18
+#
+######################################################################################
+function aggregate-scapresults{
+<#
+.SYNOPSIS
+Purpose of this script is to parse a SCAP scan and then aggregate the results into one CSV
+  __________________     _____ __________     _____    ________  __________________ 
+ /   _____/\_   ___ \   /  _  \\______   \   /  _  \  /  _____/ /  _____/\______   \
+ \_____  \ /    \  \/  /  /_\  \|     ___/  /  /_\  \/   \  ___/   \  ___ |       _/
+ /        \\     \____/    |    \    |     /    |    \    \_\  \    \_\  \|    |   \
+/_______  / \______  /\____|__  /____|     \____|__  /\______  /\______  /|____|_  /
+        \/         \/         \/                   \/        \/        \/        \/
+
+
+.DESCRIPTION
+Because I need to pull data out of a large SCAP scan so that it can be aggregated into a flat file and anaylsis
+can then be done to see what machines and CAT vulnerabities should be targeted
+
+.EXAMPLE
+aggregate-scapresults -inputdirectoryPath C:\Users\first.last\SCC\Results -outputPath C:\Users\first.last
+
+#>
+param (
+[Parameter(Mandatory=$true)]
+$inputdirectoryPath,
+[Parameter(Mandatory=$true)]
+$outputPath
+)
+# CLASSES FOR OBJECTS
+Class SCAPSCORE {
+[String]$Host
+[Int]$Score
+[DateTime]$ScanDate
+[String]$title
+}
+Class SCAPRESULTS {
+[String]$Host
+[String]$Severity
+[Int]$Weight
+[string]$Result
+[String]$STIG_Reference
+[DateTime]$ScanDate
+}
+# Get the path for the scan 
+$OS_Scans = Get-ChildItem $inputdirectoryPath -Recurse | where-object Name -CMatch "XCCDF-Results_"
+foreach ( $x in $OS_Scans ){
+# Importing the file to an xml object
+[xml]$SCAP = Get-Content -Path $x.fullname
+# Now Parsing the XML content and creating a SCAPSCORE object
+$level1 = $scap.ChildNodes
+$MachineScapScore = new-object SCAPSCORE
+$MachineScapScore.HOST = $level1.testresult.target
+$results = $level1.TestResult.score
+$MachineScapScore.Score = $results | where-object system -EQ "urn:xccdf:scoring:default" | select-object "#text" -ExpandProperty "#text"
+$MachineScapScore.ScanDate = $level1.TestResult.'end-time'
+$MachineScapScore.title = $level1.title
+$MachineScapScore | Export-Csv ($outputPath+"\SCAP_SCAN_SCORE.csv") -NoTypeInformation -Append
+# Now performing the same action for the detailed scan results
+$testresults = $level1.testresult.'rule-result'
+# Now creating the class for that object to put in the detailed scan results
+foreach ($x in $testresults){
+$MachineTestResults = New-Object SCAPRESULTS
+$MachineTestResults.Host = $level1.testresult.target
+$MachineTestResults.Severity = $x.severity
+$MachineTestResults.Weight = $x.weight
+$MachineTestResults.Result = $x.result
+$MachineTestResults.STIG_Reference = ($x.idref -replace 'xccdf_mil.disa.stig_rule_','')
+$MachineTestResults.ScanDate = $level1.TestResult.'end-time'
+$MachineTestResults | Export-Csv ($outputPath+"\SCAP_SCAN_RESULTS.csv") -NoTypeInformation -Append
+}
+}
+}
---
+++ STIG_2_CSV.ps1
@@ -0,0 +1,57 @@
+﻿######################################################################################
+#
+# Chief J's STIG-2-CSV
+# Version 2 / 01JUN18
+#
+######################################################################################
+function export-STIG2CSV{
+<#
+.SYNOPSIS
+Purpose of this script is to parse a STIG XCCDF.XML file and then convert it to an XML file.
+   _____________________     ___        ____________    __
+  / ___/_  __/  _/ ____/    |__ \      / ____/ ___/ |  / /
+  \__ \ / /  / // / __________/ /_____/ /    \__ \| | / / 
+ ___/ // / _/ // /_/ /_____/ __/_____/ /___ ___/ /| |/ /  
+/____//_/ /___/\____/     /____/     \____//____/ |___/ 
+
+
+.DESCRIPTION
+Because I need to pull data out of an XML format to a flat file to do super cool analytics. Maybe I'll get crazy and do those analytics in
+ELK. For now I am just going to put them in PowerBI to use the relational data capability.
+
+.EXAMPLE
+export-STIG2CSV -STIGInput \\PATH\U_Microsoft_IE11_V1R9_Manual-xccdf.xml -output \\path\filename.csv
+
+#>
+param(
+[Parameter(Mandatory=$true)]
+$STIGInput, 
+[Parameter(Mandatory=$true)]
+$Output
+)
+
+#Class for STIG XML Object Conversion
+Class STIGEntry {
+[String]$STIG_ID
+[String]$STIG_CHECK
+[String]$STIG_FIX
+[String]$STIG_TITLE
+[String]$STIG_WEIGHT
+[String]$STIG_SEVERITY
+}
+# Get the STIG in XML format
+[xml]$STIG  = Get-Content -Path $STIGInput
+$STIGbenchmark = $STIG.Benchmark.Group
+# Now convert the XML
+foreach ( $i in $STIGbenchmark ) {
+$export = New-Object -TypeName STIGEntry
+$export.STIG_ID = $i.rule.id
+$export.STIG_CHECK = $i.rule.check.'check-content'
+$export.STIG_FIX = $i.rule.fixtext.'#text'
+$export.STIG_TITLE = $i.rule.title
+$export.STIG_WEIGHT = $i.rule.weight
+$export.STIG_SEVERITY = $i.rule.severity
+$export | Export-Csv $Output -NoTypeInformation -Append
+Remove-Variable -Name export
+}
+}
